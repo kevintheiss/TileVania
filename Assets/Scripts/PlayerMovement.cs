4e50a62d5ebd269b74e0c623667bf68fce30d38b
GeekmasterK <ktheiss1990@gmail.com>
2023-03-20 16:20:06 -0700

    Added code to check if the player is on the ground. The player can only jump if they are on the ground, preventing infinite air jumps.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,11 +11,13 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveInput;
     Rigidbody2D playerRigidBody;
     Animator playerAnimator;
+    CapsuleCollider2D playerCapsuleCollider;
 
     void Start()
     {
         playerRigidBody = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
+        playerCapsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
@@ -32,6 +34,11 @@ public class PlayerMovement : MonoBehaviour
 
     void OnJump(InputValue value)
     {
+        if(!playerCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+
         if(value.isPressed)
         {
             playerRigidBody.velocity += new Vector2(0f, jumpSpeed);
