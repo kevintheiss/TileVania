02638f9e993d7214885006e270fa4c254159092a
GeekmasterK <ktheiss1990@gmail.com>
2023-03-22 16:40:16 -0700

    Added a player death animation and physics to make the player fly through the air on death.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float jumpSpeed = 5f;
     [SerializeField] float climbSpeed = 5f;
+    [SerializeField] Vector2 deathKick = new Vector2(10f, 10f);
 
     Vector2 moveInput;
     Rigidbody2D playerRigidBody;
@@ -114,6 +115,8 @@ public class PlayerMovement : MonoBehaviour
         if(playerBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemies")))
         {
             isAlive = false;
+            playerAnimator.SetTrigger("Dying");
+            playerRigidBody.velocity = deathKick;
         }
     }
 }
