7308b38179a074546a1fe5d496661240138e7a79
GeekmasterK <ktheiss1990@gmail.com>
2023-03-28 12:17:50 -0700

    Created a scene persistence prefab to control the game state. Coin and enemy states remain when the player loses a life, and reset when the game resets.

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -18,6 +18,7 @@ public class LevelExit : MonoBehaviour
             nextSceneIndex = 0;
         }
 
+        FindObjectOfType<ScenePersist>().ResetScenePersist();
         SceneManager.LoadScene(nextSceneIndex);
     }
 
